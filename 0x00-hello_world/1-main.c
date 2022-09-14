079604a00b044a3a282d06fdc0ac206bb659966d
wangui <wanguijane468@gmail.com>
2022-09-14 12:45:14 -0700

    betty checker error corrections

--- 0x00-hello_world/1-main.c
+++ 0x00-hello_world/1-main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-
+/**
+ * Main -Entry Point
+ * Description: 'THis is a program to print a simple message'
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{
+}
 	printf("My output statement");
 
-	return(0);
+	return (0);
 }
