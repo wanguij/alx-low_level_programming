59fb3df191de34683483ea925461706b65d8b4fa
wangui <wanguijane468@gmail.com>
2022-09-19 15:47:02 -0700

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * function to take value of a pointer int 
+ * and reset the value of the pointer to 98
+ *
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
+
+
---
+++ 0x05-pointers_arrays_strings/main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	n = 402;
+
+}
